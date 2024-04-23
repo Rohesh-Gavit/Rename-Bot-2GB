2bdf930b488d62577607969e791ab6bd13e7a159
Rohesh Gavit <153485430+Rohesh-Gavit@users.noreply.github.com>
2024-04-23 23:30:03 +0530

    Update thumbnail.py

--- plugins/thumbnail.py
+++ plugins/thumbnail.py
@@ -8,17 +8,15 @@ async def viewthumb(client, message):
     if thumb:
        await client.send_photo(chat_id=message.chat.id, photo=thumb)
     else:
-        await message.reply_text("**You Don't Have Any Thumbnail ❌**") 
+        await message.reply_text("😔 __**Yᴏᴜ Dᴏɴ'ᴛ Hᴀᴠᴇ Aɴy Tʜᴜᴍʙɴᴀɪʟ**__") 
 		
 @Client.on_message(filters.private & filters.command(['del_thumb', 'delthumb']))
 async def removethumb(client, message):
     await roheshbots.set_thumbnail(message.from_user.id, file_id=None)
-    await message.reply_text("**Thumbnail Deleted Successfully 🗑️**")
+    await message.reply_text("❌️ __**Tʜᴜᴍʙɴᴀɪʟ Dᴇʟᴇᴛᴇᴅ**__")
 	
 @Client.on_message(filters.private & filters.photo)
 async def addthumbs(client, message):
-    roheshbots = await message.reply_text("Please Wait ...")
+    mkn = await message.reply_text("Please Wait ...")
     await roheshbots.set_thumbnail(message.from_user.id, file_id=message.photo.file_id)                
-    await roheshbots.edit("**Thumbnail Saved Successfully ✅️**")
-
-
+    await mkn.edit("✅️ __**Tʜᴜᴍʙɴᴀɪʟ Sᴀᴠᴇᴅ**__")
